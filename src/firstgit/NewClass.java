3276533188d8f50ebd70e21ad906e717e91f73b6
bstoiber <bstoiber@L11706042769.wctc.msft>
2017-01-26 21:04:08 -0600

    Added a variable and changed the message.

--- src/firstgit/NewClass.java
+++ src/firstgit/NewClass.java
@@ -9,15 +9,6 @@ package firstgit;
  *
  * @author bstoiber
  */
-public class FirstGit {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("hello java");
-        int x = 5; 
-    }
+public class NewClass {
     
 }
